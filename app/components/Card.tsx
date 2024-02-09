8535d1e5662ea6a0fafb376743b09566c66c6370
Barbara Peric <b.peric10@gmail.com>
2024-02-09 12:54:56 +0100

    card list component

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,11 +1,26 @@
 'use client'
 
-const Card = (title: string) => {
+import { motion } from "framer-motion";
+
+const Card = ({ colIndex, rowIndex, size, gap }) => {
+  const diagonalIndex = (360 / 6) * (colIndex + rowIndex);
+
+
   return (
-    <div>
-      <p>{title}</p>
-    </div>
-  )
-}
+    <motion.div
+      style={{
+        boxShadow: `2px 2px 2px 0px rgba(0, 0, 0, .3)`,
+        background: `hsla(calc(var(--base-hue)), 90%, 40%, 1)`,
+        width: size,
+        height: size,
+        top: rowIndex * (size + gap),
+        left: colIndex * (size + gap),
+        position: "absolute",
+        borderRadius: "20%",
+      }}
+    />
+  );
+};
+
 
 export default Card
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Image from 'next/image'
+import CardList from './components/CardList'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='grid grid-cols-3 gap-4'>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-10">
+        <CardList />
     </main>
   )
 }
