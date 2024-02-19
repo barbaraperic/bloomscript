a31f9fed602c4738ae6591efc2271675e11f61df
Barbara Peric <b.peric10@gmail.com>
2024-02-19 11:46:20 +0000

    style fiexs

--- app/immutable/[id]/page.tsx
+++ app/immutable/[id]/page.tsx
@@ -8,16 +8,8 @@ export default function ImmutableItemPage({
 }) {
     const { id: title } = params
     return (
-        <div className="flex flex-col h-screen items-center justify-center bg-amber-300 gap-10">
-            <h1 className="text-4xl pageHeader">Contact Page</h1>
-            <p className="mx-10 pageContent text-center line-clamp-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
-                urna auctor, viverra sapien. Donec euismod turpis eget massa
-                lobortis, eget scelerisque justo.
-            </p>
-        </div>
-        // <main className="page-header animate-slide-up w-full h-full bg-orange-100 flex min-h-screen flex-col items-center p-10 space-y-5">
-        //     <Modal title={title} />
-        // </main>
+        <main className="w-full h-full flex min-h-screen flex-col items-center p-10 space-y-5">
+            <Modal title={title} />
+        </main>
     )
 }
