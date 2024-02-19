3981d8b357223bda518e0e3117dc7e458d647cbb
Barbara Peric <b.peric10@gmail.com>
2024-02-19 16:07:56 +0000

    fix title

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -5,15 +5,15 @@ import { motion } from 'framer-motion'
 const Title = ({ children }: { children: React.ReactNode }) => {
     return (
         <motion.div
-            animate={{ '--base-hue': 210 } as any}
-            initial={{ '--base-hue': 190 } as any}
+            animate={{ '--base-saturation': 100 } as any}
+            initial={{ '--base-saturation': 0 } as any}
             transition={{ duration: 10, loop: Infinity, ease: 'linear' }}
             style={{ width: '100%', height: '100%', textAlign: 'center' }}
         >
             <h2
                 style={{
                     cursor: 'pointer',
-                    color: `hsla(calc(var(--base-hue)), 100%, 50%, 1)`,
+                    color: `hsla(180, calc(var(--base-saturation) * 1%), 40%, 1)`,
                     fontSize: '36px',
                     textTransform: 'uppercase',
                 }}
