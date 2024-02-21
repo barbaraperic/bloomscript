8c5d6e35e6ed966479d5f7a26d48910600a3ece3
Barbara Peric <b.peric10@gmail.com>
2024-02-21 12:19:13 +0000

    start mdx project

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,22 +1,25 @@
 'use client'
 import { FiX } from 'react-icons/fi'
 import useAnimatedRouter from '../hooks/useAnimatedRouter'
+import { usePathname } from 'next/navigation'
 
-export default function Modal({
-    title,
-    path,
-}: {
-    title: string
-    path?: string
-}) {
+export default function Modal({ title }: { title: string }) {
     const { animatedRoute } = useAnimatedRouter()
+
+    const paths = usePathname()
+
+    const path = paths
+        .split('/')
+        .filter((el) => el !== title)
+        .filter(Boolean)
+        .join()
 
     return (
         <div className="w-full h-full flex justify-center">
             <h2 className="mt-10">{title}</h2>
             <button
                 className="absolute right-4 top-4"
-                onClick={() => animatedRoute('/iterators')}
+                onClick={() => animatedRoute(`/${path}`)}
             >
                 <FiX className="w-6 h-6 text-white" />
             </button>
